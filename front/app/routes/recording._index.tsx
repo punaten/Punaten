fd3fd2aab5eb331ac7a22a6a74731200ffbb0249
YukaChoco <sugar.ore.yk@gmail.com>
2024-03-18 20:37:48 +0900

    webm形式での保存完了

--- front/app/routes/recording._index.tsx
+++ front/app/routes/recording._index.tsx
@@ -20,6 +20,60 @@ export default function Index() {
     }
   }, [webcamRef]);
 
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const [capturing, setCapturing] = useState<boolean>(false);
+  const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
+
+  const handleStartCaptureClick = useCallback(() => {
+    setCapturing(true);
+    if (webcamRef.current) {
+      mediaRecorderRef.current = new MediaRecorder(
+        webcamRef.current.stream as MediaStream,
+        {
+          mimeType: "video/webm",
+        }
+      );
+      mediaRecorderRef.current.addEventListener(
+        "dataavailable",
+        handleDataAvailable
+      );
+      mediaRecorderRef.current.start();
+    }
+  }, [webcamRef, setCapturing, mediaRecorderRef]);
+
+  const handleDataAvailable = useCallback(
+    ({ data }: { data: Blob }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => [...prev, data]);
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStopCaptureClick = useCallback(() => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+    }
+    setCapturing(false);
+  }, [mediaRecorderRef, setCapturing]);
+
+  const handleDownload = useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style.display = "none";
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+      a.click();
+      URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
   return (
     <Center bg={["cream-dark", "dark"]} h={"full"}>
       <DarkModeController />
@@ -36,14 +90,15 @@ export default function Index() {
             <button onClick={() => setCaptureEnable(false)}>終了</button>
           </div>
           <div>
-            <Webcam
-              audio={false}
-              width={360}
-              height={640}
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              videoConstraints={videoConstraints}
-            />
+            <Webcam audio={false} ref={webcamRef} />
+            {capturing ? (
+              <button onClick={handleStopCaptureClick}>Stop Capture</button>
+            ) : (
+              <button onClick={handleStartCaptureClick}>Start Capture</button>
+            )}
+            {recordedChunks.length > 0 && (
+              <button onClick={handleDownload}>Download</button>
+            )}
           </div>
           <button onClick={capture}>キャプチャ</button>
         </>
