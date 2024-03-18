ef45a19cd57c99dbc3d15e34ee7858eacb5d1f01
YukaChoco <sugar.ore.yk@gmail.com>
2024-03-18 14:55:48 +0900

    darkmodeでテキストの色を変える

--- front/app/routes/_index.tsx
+++ front/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
-import { Box } from "@yamada-ui/react";
+import { Box, Button, Wrap, useColorMode } from "@yamada-ui/react";
 export const meta: MetaFunction = () => {
   return [
     { title: "ダンスDE猫ミーム" },
@@ -11,5 +11,18 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return <Box color={"primary.200"}>Hello World!</Box>;
+  const { colorMode, changeColorMode, toggleColorMode } = useColorMode();
+  return (
+    <Box>
+      <Wrap gap="md" position={"fixed"} bottom={0} padding={10}>
+        <Button onClick={() => changeColorMode("light")}>ライトモード</Button>
+        <Button onClick={() => changeColorMode("dark")}>ダークモード</Button>
+        <Button onClick={() => changeColorMode("system")}>システム</Button>
+        <Button onClick={toggleColorMode}>
+          {colorMode === "light" ? "ダーク" : "ライト"}モードに切り替える
+        </Button>
+      </Wrap>
+      <Box color={["blue.700", "primary.400"]}>Hello World!</Box>
+    </Box>
+  );
 }
