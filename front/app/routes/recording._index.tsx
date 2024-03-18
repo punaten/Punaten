f375cb8aad379f45d7d6acfa418278daa5478924
YukaChoco <sugar.ore.yk@gmail.com>
2024-03-18 20:06:01 +0900

    cameraの起動・表示

--- front/app/routes/recording._index.tsx
+++ front/app/routes/recording._index.tsx
@@ -1,12 +1,69 @@
 import { Center, Flex, Link, Motion } from "@yamada-ui/react";
 import DarkModeController from "~/components/global/DarkModeController";
 import SubLogo from "~/components/global/SubLogo";
+import { useRef, useState, useCallback } from "react";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 360,
+  height: 720,
+};
 
 export default function Index() {
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+    }
+  }, [webcamRef]);
+
   return (
-    <Center bg={["cream-dark", "dark"]} h={"full"} >
+    <Center bg={["cream-dark", "dark"]} h={"full"}>
       <DarkModeController />
-      <SubLogo/>
+      <SubLogo />
+      <header>
+        <h1>カメラアプリ</h1>
+      </header>
+      {isCaptureEnable || (
+        <button onClick={() => setCaptureEnable(true)}>開始</button>
+      )}
+      {isCaptureEnable && (
+        <>
+          <div>
+            <button onClick={() => setCaptureEnable(false)}>終了</button>
+          </div>
+          <div>
+            <Webcam
+              audio={false}
+              width={360}
+              height={640}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+          </div>
+          <button onClick={capture}>キャプチャ</button>
+        </>
+      )}
+      {url && (
+        <>
+          <div>
+            <button
+              onClick={() => {
+                setUrl(null);
+              }}
+            >
+              削除
+            </button>
+          </div>
+          <div>
+            <img src={url} alt="Screenshot" />
+          </div>
+        </>
+      )}
     </Center>
   );
 }
