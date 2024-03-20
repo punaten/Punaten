0fb6b6da9ddc9a600db2fc166f118c28a4fa9739
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2024-03-20 13:21:59 +0900

    トップに戻るボタン

--- front/app/components/global/SubLogo.tsx
+++ front/app/components/global/SubLogo.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@yamada-ui/react";
-
+import { Link } from "@remix-run/react";
 const SubLogo = () => {
   return (
     <Box position={"fixed"} top={4} left={4} zIndex={20}>
-      <img src="/sub_logo.webp" alt="ダンスDE猫ミームのサブロゴ" width={274} height={56} />
+      <Link to={"/"}>
+        <img src="/sub_logo.webp" alt="ダンスDE猫ミームのサブロゴ" width={274} height={56} />
+      </Link>
     </Box>
   );
 };
