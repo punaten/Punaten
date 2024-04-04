7af84d67a7bb7679ad606312d9fc0d94d8960fb0
YukaChoco <sugar.ore.yk@gmail.com>
2024-04-05 03:03:29 +0900

    その他気になったデザイン修正

--- front/app/routes/howToPlay.tsx
+++ front/app/routes/howToPlay.tsx
@@ -1,55 +1,56 @@
-import { Button, Center, Container, Motion, Text } from "@yamada-ui/react";
+import { Center, Container, Text } from "@yamada-ui/react";
 import { Link } from "@remix-run/react";
 import MotionButton from "~/components/global/MotionButton";
 
 const HowToPlay = () => {
   return (
-    <Container>
-      <Container
-        mt={"5rem"}
-        py={"2rem"}
-        bg={["cream-light", "dark-caramel"]}
-        border={"0.25rem solid"}
-        borderColor={["cinnamon", "dark-caramel"]}
-        rounded={"0.5rem"}
-      >
-        <Text
-          as={"h1"}
-          fontSize={"3rem"}
-          w={"fit-content"}
-          m={"auto"}
-          color={["cinnamon", "caramel"]}
+    <Center h={"full"}>
+      <Container>
+        <Container
+          mt={"5rem"}
+          bg={["cream-light", "dark-caramel"]}
+          border={"0.25rem solid"}
+          borderColor={["cinnamon", "dark-caramel"]}
+          rounded={"0.5rem"}
         >
-          遊び方
-        </Text>
-        <Center>
           <Text
-            className="description"
-            text={"2rem"}
+            as={"h1"}
+            fontSize={"3rem"}
+            w={"fit-content"}
+            m={"auto"}
             color={["cinnamon", "caramel"]}
           >
-            Startボタンを押すと30秒の録画が始まります。
-            <br />
-            全力で猫ミームの真似をしてください。
-            <br />
-            全力で踊ってください。
-            <br />
-            30秒経過すると自動的に録画が終了し、猫ミーム
-            <br />
-            の生成が始まります。
+            遊び方
           </Text>
-        </Center>
+          <Center>
+            <Text
+              className="description"
+              text={"2rem"}
+              color={["cinnamon", "caramel"]}
+            >
+              Startボタンを押すと30秒の録画が始まります。
+              <br />
+              全力で猫ミームの真似をしてください。
+              <br />
+              全力で踊ってください。
+              <br />
+              30秒経過すると自動的に録画が終了し、猫ミーム
+              <br />
+              の生成が始まります。
+            </Text>
+          </Center>
+        </Container>
+        <Container>
+          <Link
+            to="/recording"
+            className="start-btn"
+            style={{ width: "fit-content", margin: "0 auto" }}
+          >
+            <MotionButton onClick={() => {}}>Start</MotionButton>
+          </Link>
+        </Container>
       </Container>
-      <Container>
-        <Link
-          to="/recording"
-          className="start-btn"
-          style={{ width: "fit-content", margin: "0 auto" }}
-        >
-          <MotionButton onClick={() => {}}>Start</MotionButton>
-        </Link>
-      </Container>
-    </Container>
+    </Center>
   );
 };
 
